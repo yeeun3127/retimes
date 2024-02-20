c69edfae798afc9936c063c5803a49eec656699a
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-21 02:18:45 +0900

    thrid

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const API_KEY = `802b0b4ba9d44b078e2a3d0925cc42cb`;
 let news = [];
 const getLatestNews = async () => {
-  const url = new URL(`https://re-times.netlify.app/`);
-  console.log(url);
+  const url = new URL(
+    `https://re-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;

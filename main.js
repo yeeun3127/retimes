ae16d27438072ad77c1848ceeeb0ac4c8c7fd081
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-21 02:34:00 +0900

    dd

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const API_KEY = `802b0b4ba9d44b078e2a3d0925cc42cb`;
+const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const url2 = `https://re-times.netlify.app/`;
 let news = [];
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://re-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
-  );
+  const url = new URL(url2);
 
   const response = await fetch(url);
   const data = await response.json();

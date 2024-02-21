28c46cf05b111115b840ad89d5c2047930785bdf
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-22 01:42:40 +0900

    html.css

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
 const API_KEY = `802b0b4ba9d44b078e2a3d0925cc42cb`;
 const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 const url2 = "https://re-times.netlify.app/top-headlines";
-let news = [];
+let newsList = [];
 const getLatestNews = async () => {
   const url = new URL(url2);
 
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("ddddd", data.articles);
+  newsList = data.articles;
+  render();
+  console.log("ddddd", newsList);
+};
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+  <div class="col-lg-4">
+    <img class"news-img-size" src=${news.urlToImage} />
+  </div>
+  <div class="col-lg-8">
+    <h2>${news.title}</h2>
+    <p>${news.description}</p>
+    <div>${news.source.name} * ${news.publishedAt}</div>
+  </div>
+</div>`
+    )
+    .join(" ");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();

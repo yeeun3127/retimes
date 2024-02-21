98be8810dbae6cd61a1023f65e0170d6cedf8bcb
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-21 22:19:57 +0900

    /top-headlines

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_KEY = `802b0b4ba9d44b078e2a3d0925cc42cb`;
 const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-const url2 = `https://re-times.netlify.app/`;
+const url2 = "https://re-times.netlify.app/top-headlines";
 let news = [];
 const getLatestNews = async () => {
   const url = new URL(url2);

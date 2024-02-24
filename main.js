2ebbd86ae5a851f3a494069533ad8977e80579b5
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-25 02:55:00 +0900

    html & css 정리 및 js pagination

--- main.js
+++ main.js
@@ -7,92 +7,120 @@ menus.forEach((menu) =>
   menu.addEventListener("click", (event) => getNewsByCategory(event))
 );
 let url = new URL(`https://re-times.netlify.app/top-headlines`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
-const getNews = async () => {
+// 메뉴 클릭
+menus.forEach((menu) =>
+  menu.addEventListener("click", (e) =>
+    fetchNews({ category: e.target.textContent.toLowerCase() })
+  )
+);
+
+// 검색
+document.getElementById("search-button").addEventListener("click", () => {
+  const keyword = document.getElementById("search-input").value;
+  fetchNews({ keyword });
+});
+
+async function fetchNews({ category = "", keyword = "" } = {}) {
+  let baseurl = new URL(url2);
+  if (category) baseurl += `&category=${category}`;
+  if (keyword) baseurl += `&q=${keyword}`;
+
   try {
-    const response = await fetch(url);
+    baseurl.searchParams.set("page", page); // &page=page
+    baseurl.searchParams.set("pageSize", pageSize); // &pageSize=pageSize
+    const response = await fetch(baseurl);
     const data = await response.json();
     if (response.status === 200) {
       if (data.articles.length === 0) {
         throw new Error("No result for this search");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     } else {
       throw new Error(data.message);
     }
   } catch (error) {
     errorRender(error.message);
   }
-};
-
-const getLatestNews = async () => {
-  url = new URL(url2);
-
-  getNews();
-  console.log("ddddd", newsList);
-};
-
-const getNewsByCategory = async (event) => {
-  const category = event.target.textContent.toLowerCase();
-  console.log("category");
-  url = new URL(
-    `https://re-times.netlify.app/top-headlines?country=kr&category=${category}`
-  );
-  getNews();
-};
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
 }
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-const openSearchBox = () => {
-  let inputArea = document.getElementById("input-area");
-  if (inputArea.style.display === "inline") {
-    inputArea.style.display = "none";
-  } else {
-    inputArea.style.display = "inline";
-  }
-};
-
-const getNewsBykeyword = async () => {
-  const keyword = document.getElementById("search-input");
-  console.log("keyword");
-  url = new URL(
-    `https://re-times.netlify.app/top-headlines?country=kr&q=${keyword}`
-  );
-  getNews();
-};
 
 const render = () => {
+  // array function
   const newsHTML = newsList
     .map(
-      (news) => `<div class="row news">
-  <div class="col-lg-4">
-    <img class"news-img-size" src=${news.urlToImage} />
-  </div>
-  <div class="col-lg-8">
-    <h2>${news.title}</h2>
-    <p>${news.description}</p>
-    <div>${news.source.name} * ${news.publishedAt}</div>
-  </div>
-</div>`
+      (
+        news
+      ) => `<div class="row news"><div class="col-lg-4"><img class="news-img-size" src=${
+        news.urlToImage ||
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+      }>
+                  </div>
+                  <div class="col-lg-8">
+                  <h3>${news.title}</h3>
+                    <p>
+                      ${
+                        news.description == null || news.description == ""
+                          ? "내용없음"
+                          : news.description.length > 200
+                          ? news.description.substring(0, 200) + "..."
+                          : news.description
+                      }
+                                                </p>
+                                                <div>
+                                                    ${
+                                                      news.source.name ||
+                                                      "No source"
+                                                    } * ${moment(
+        news.publishedAt
+      ).fromNow()}
+                                                </div>
+                                            </div>
+                                        </div>`
     )
-    .join(" ");
+    .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 const errorRender = (errorMessage) => {
   const errorHTML = `<div class="alert alert-danger" role="alert">
-  ${errorMessage}
-</div>`;
+                        ${errorMessage}
+                    </div>`;
   document.getElementById("news-board").innerHTML = errorHTML;
 };
 
-getLatestNews();
+const paginationRender = () => {
+  const totalPages = Math.ceil(totalResults / pageSize);
+  const pageGroup = Math.ceil(page / groupSize);
+  const lastPage = pageGroup * groupSize;
+  if (lastPage > totalPages) {
+    // 마지막 페이지 그룹이 그룹 사이즈보다 작다? lastpage = totalpage
+    lastPage = totalPages;
+  }
+  const firstPage =
+    lastPage - (groupSize - 1) < 0 ? 1 : lastPage - (groupSize - 1);
+
+  // first~last : bootstrap!!!
+  let paginationHTML = ``;
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += `<li class="page-item ${
+      i === page ? "active" : ""
+    }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  }
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  console.log("moveToPage", pageNum);
+  page = pageNum;
+  fetchNews();
+};
+
+fetchNews();
